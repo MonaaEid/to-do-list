7b37ec9d2220e246f9ffef7ac6ab4480ccf380d3
MonaaEid <monaa_eid@hotmail.com>
2022-12-28 20:18:27 +0200

    Initial commit

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,10 +1,13 @@
 <script>
+import ToDoItemEditForm from "./ToDoItemEditForm.vue";
 // import uniqueId from 'lodash.uniqueid';
 export default { 
     // name: 'app',
 
-//     components: {ToDoItem
-// },
+    components: {
+  ToDoItemEditForm
+},
+
 props: { 
     label: { required: true, type: String },
     done: { default: false, type: Boolean },
@@ -13,17 +16,40 @@ props: {
 data() {
     return {
       isDone: this.done,
+      isEditing: false
     };
+  },
+  methods: {
+    deleteToDo() {
+      this.$emit('item-deleted');
+    },
+    toggleToItemEditForm() {
+      this.isEditing = true;
+    }
   }
+
 };
 </script>
 <template>
-  
-<div class="custom-checkbox" >
-    <input type="checkbox" :id="id"  v-bind:checked="isDone"   @change="$emit('checkbox-changed')"/>
-    <label for="todo-item">{{label}}</label>
-</div>
+  <div  class="stack-small" v-if="!isEditing">
+    <div class="custom-checkbox">
+      <input type="checkbox" class="checkbox" :id="id" :checked="isDone"
+             @change="$emit('checkbox-changed')" />
+      <label :for="id" class="checkbox-label">{{label}}</label>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn"  @click="toggleToItemEditForm">
+        Edit <span class="visually-hidden">{{label}}</span>
+      </button>
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete <span class="visually-hidden">{{label}}</span>
+      </button>
+    </div>
+  </div>
+  <to-do-item-edit-form v-else :id="id" :label="label"></to-do-item-edit-form>
+
 </template>
+
 <style scoped>
 .custom-checkbox > .checkbox-label {
   font-family: Arial, sans-serif;
